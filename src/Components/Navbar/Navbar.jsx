de4cf011e29ef3564e1b2b62ce0ee6615e5451e6
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-04-23 21:21:48 +0100

    Notification tab

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Notification from "../Notification/Notification";
+
+const Navbar = () => {
+	return (
+		<div className="app__navbar">
+			<Notification />
+		</div>
+	);
+};
+
+export default Navbar;
---
+++ src/Components/Notification/Notification.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Notification.scss";
+
+const Notification = () => {
+	return (
+		<div className="app__notification">
+			<div className="app__notification_content">
+				<p>Welcome to LearnDev Foundation</p>
+			</div>
+		</div>
+	);
+};
+
+export default Notification;
