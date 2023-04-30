897584f0c4586c8f74d1658e1f6be2a675fac9e5
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-04-30 22:59:19 +0100

    Roadmap page

--- src/Container/Map/Map.jsx
+++ src/Container/Map/Map.jsx
@@ -1,18 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { Navbar, Footer } from "../../Components";
+
+import "./Map.scss";
 
 const Map = () => {
 	const location = useLocation();
 	const { data } = location.state;
 
-	const [roadmap, setRoadmap] = useState(null);
-	import(`./Roadmaps/${data}.json`).then((map) => {
-		setRoadmap(map.default);
-	});
-	console.log(roadmap);
+	const [roadmap, setRoadmap] = useState("");
+	
+	useEffect(() => {
+		import(`./Roadmaps/${data}.json`).then((map) => {
+			setRoadmap(map);
+		});
+	}, []);
+
+	const mapData = roadmap.roadmap;
+
+	if(!roadmap) {
+		return <div>Loading....</div>;
+	}
 
 	return (
-		<div>Map</div>
+		<div>
+			<Navbar />
+			<div className="app__map">
+				<div className="app__map_title">
+					<h2>{roadmap.name}</h2>
+					<p>{roadmap.description}</p>
+				</div>
+				<ol className="app__map_roadmap">
+					{Object.keys(mapData).map((key) => {
+						const item = mapData[key];
+						if (typeof item === "string") {
+							return <li key={key}>{item}</li>;
+						} else {
+							return (
+								<li key={key}>
+									<h3>{item.topic}</h3>
+									{item.recommendations && (
+										<>
+											<h4>Recommendations:</h4>
+											<ul>
+												{item.recommendations.map((rec) => (
+													<li key={rec}>{rec}</li>
+												))}
+											</ul>
+										</>
+									)}
+									{item.articles && (
+										<>
+											<h4>Articles:</h4>
+											<ul>
+												{item.articles.map((article) => (
+													<li key={article.url}>
+														<a href={article.url}>{article.title}</a>
+													</li>
+												))}
+											</ul>
+										</>
+									)}
+								</li>
+							);
+						}
+					})}
+				</ol>
+			</div>
+			<Footer />
+		</div>
 	);
 };
 
